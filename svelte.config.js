437f2a97d84df8f463ee89a83fdb360474a51cd9
Your Name <you@example.com>
2026-02-20 23:58:09 +0530

    nothing

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      // This tells the adapter "I know what I'm doing, use Node 24"
+      runtime: 'nodejs24.x' 
+    })
   }
 };
 
